67d2828e40cc1b6359255ad1681209452f86808e
Fernando Carvalho <nando_carvalhoo@hotmail.com>
2024-07-03 23:28:28 -0300

    implemented needed tables to entity and created the vocations enum

--- src/players/player.entity.ts
+++ src/players/player.entity.ts
@@ -26,6 +26,39 @@ export class Player {
   @Column()
   town_id: number;
 
+  @Column({ default: 1 })
+  level: number;
+
+  @Column({ default: 0 })
+  experience: number;
+
+  @Column({ default: 0 })
+  maglevel: number;
+
+  @Column({ default: 10 })
+  skill_fist: number;
+
+  @Column({ default: 10 })
+  skill_club: number;
+
+  @Column({ default: 10 })
+  skill_sword: number;
+
+  @Column({ default: 10 })
+  skill_axe: number;
+
+  @Column({ default: 10 })
+  skill_dist: number;
+
+  @Column({ default: 10 })
+  skill_shielding: number;
+
+  @Column({ default: 10 })
+  skill_fishing: number;
+
+  @Column({ default: 0 })
+  boss_points: number;
+
   @ManyToOne(() => Account, (account) => account.players)
   @JoinColumn({ name: 'account_id' })
   account: Account;
