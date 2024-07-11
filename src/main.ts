1a2b4c2e91085ffa54914f744d92dbedecee3132
Fernando Carvalho <nando_carvalhoo@hotmail.com>
2024-07-11 01:19:44 -0300

    Made changes in swagger documentation so now it's possible to get a bearer token, authorize there and use some protected endpoint

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,6 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import * as dotenv from 'dotenv';
 import { ValidationPipe } from '@nestjs/common';
 import { AllExceptionsFilter } from './common/filters/all-exceptions.filter';
-import * as cors from 'cors';
 
 dotenv.config();
 
@@ -13,26 +12,24 @@ async function bootstrap() {
 
   app.enableCors({
     origin: '*',
-    // origin: [
-    //   'https://f3s-checkout.netlify.app/',
-    //   'https://donate.whitekingdomot.com',
-    //   'http://donate.whitekingdomot.com',
-    // ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: false,
   });
 
-  // Setup Swagger
   const config = new DocumentBuilder()
     .setTitle('F3S Games API')
     .setDescription('API for F3S Games services and applications')
     .setVersion('1.0')
     .addTag('f3sgames')
+    .addBearerAuth(
+      { type: 'http', scheme: 'bearer', bearerFormat: 'JWT' },
+      'access-token',
+    )
     .build();
+
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
 
-  // Apply global validation pipe
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -41,7 +38,6 @@ async function bootstrap() {
     }),
   );
 
-  // Apply global exception filter
   app.useGlobalFilters(new AllExceptionsFilter());
 
   await app.listen(3000);
