b76e757ab926ddba8e316ec4bc732a5071b0eda9
Fernando Carvalho <nando_carvalhoo@hotmail.com>
2024-07-18 17:46:50 -0300

    .

--- src/world-changes/rashid/rashid.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { RashidController } from './rashid.controller';
-import { RashidLocationService } from './rashid.service';
-
-@Module({
-  controllers: [RashidController],
-  providers: [RashidLocationService],
-})
-export class RashidModule {}
